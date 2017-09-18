4047a9d8e4bbb1f00b2c9fe6de36429ec3d69657
Patriarx666 <global300@bk.ru>
2017-09-18 14:57:23 +0300

    Add files via upload

---
+++ Patriarx GENERATOR/Patriarx GENERATOR/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Patriarx_GENERATOR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Textmail = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pbMails = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // Textmail
+            // 
+            this.Textmail.Location = new System.Drawing.Point(12, 37);
+            this.Textmail.Name = "Textmail";
+            this.Textmail.Size = new System.Drawing.Size(256, 20);
+            this.Textmail.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Сгенерировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите почту, например qwerty@mail.com\r\n";
+            // 
+            // pbMails
+            // 
+            this.pbMails.Location = new System.Drawing.Point(131, 63);
+            this.pbMails.Name = "pbMails";
+            this.pbMails.Size = new System.Drawing.Size(137, 38);
+            this.pbMails.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 113);
+            this.Controls.Add(this.pbMails);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Textmail);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(296, 152);
+            this.MinimumSize = new System.Drawing.Size(296, 152);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Patriarx GENERATOR";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Textmail;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Patriarx GENERATOR/Patriarx GENERATOR/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace Patriarx_GENERATOR
+{
+    public partial class Form1 : Form
+    {
+        private string akk;
+        private string[] akkParts;
+        private BackgroundWorker bwMakeMails;
+        private FileInfo fileForSafe;
+        private ProgressBar pbMails;
+        public Form1()
+        {
+            InitializeComponent();
+            this.bwMakeMails = new BackgroundWorker();
+            this.bwMakeMails.DoWork += new DoWorkEventHandler(this.Go);
+            this.bwMakeMails.ProgressChanged += new ProgressChangedEventHandler(this.ProgressChanged);
+            this.bwMakeMails.RunWorkerCompleted += new RunWorkerCompletedEventHandler(this.EndWork);
+            this.bwMakeMails.WorkerReportsProgress = true;
+            this.pbMails.Style = ProgressBarStyle.Continuous;  
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {  
+
+            //начало кода
+            if (!this.bwMakeMails.IsBusy)
+            {
+                this.akk = this.Textmail.Text;
+                this.akkParts = this.akk.Split(new char[] { '@' });
+                if (this.akkParts.Length != 2)
+                {
+                    MessageBox.Show("Ой, вы наверное ошиблись? \r\nПридерживайтесь формата qwe@mail.com", "Ошибка!");
+                }
+                else if (this.akkParts[0].Length < 2)
+                {
+                    MessageBox.Show("Не маловато-ли будет?", "Ошибка!");
+                }
+                else if ((this.akkParts[0].Length <= 0x15) || (MessageBox.Show("Много букав, вы уверены, что хотите ждать?", "Слишком длинная почта!", MessageBoxButtons.OKCancel) != DialogResult.Cancel))
+                {
+                    this.pbMails.Value = 0;
+                    SaveFileDialog dialog = new SaveFileDialog
+                    {
+                        Title = "Сохранение файла с мылами",
+                        DefaultExt = "txt",
+                        Filter = "Text files  (*.txt)|*.txt",
+                        FileName = this.akkParts[0]
+                    };
+                    if (dialog.ShowDialog() == DialogResult.OK)
+                    {
+                        this.fileForSafe = new FileInfo(dialog.FileName);
+                        this.bwMakeMails.RunWorkerAsync(this.akkParts[0]);
+                    }
+                }
+            }
+             
+        }
+        
+       
+        
+
+
+        
+        private void EndWork(object sender, RunWorkerCompletedEventArgs e)
+        {
+            try
+            {
+                File.WriteAllLines(this.fileForSafe.FullName, (string[])e.Result);
+                MessageBox.Show("Сгенерировано " + ((string[])e.Result).Length + " почт", "Готово");
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+        
+
+        
+
+        
+
+        private void Go(object sender, DoWorkEventArgs e)
+        {
+            string argument = (string)e.Argument;
+            int stepen = argument.Length - 1;
+            long num2 = this.Pow2(stepen) - 1L;
+            string[] strArray = new string[num2];
+            double num3 = num2;
+            int num4 = 0;
+            for (long i = 1L; i <= num2; i += 1L)
+            {
+                int num6 = 0;
+                long num7 = i;
+                strArray[(int)((IntPtr)(i - 1L))] = this.akk;
+                for (int j = stepen; j > -1; j--)
+                {
+                    long num9 = this.Pow2(j);
+                    if ((num7 / num9) == 1L)
+                    {
+                        strArray[(int)((IntPtr)(i - 1L))] = strArray[(int)((IntPtr)(i - 1L))].Insert((num6++ + stepen) - j, ".");
+                        num7 -= num9;
+                    }
+                }
+                int percentProgress = Convert.ToInt32((double)((((double)i) / num3) * 1000.0));
+                if (num4 < percentProgress)
+                {
+                    this.bwMakeMails.ReportProgress(percentProgress);
+                    num4 = percentProgress;
+                }
+            }
+            e.Result = strArray;
+        }
+
+        
+
+        private long Pow2(int stepen)
+        {
+            long num = 1L;
+            for (byte i = 0; i < stepen; i = (byte)(i + 1))
+            {
+                num *= 2L;
+            }
+            return num;
+        } 
+
+        private void ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            try
+            {
+                this.pbMails.Value = e.ProgressPercentage;
+            }
+            catch (Exception)
+            {
+            }
+        } 
+    }
+}
