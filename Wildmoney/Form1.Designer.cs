9761c85476853c56d6fc0775e1e9b6fa31f2fdf5
Patriarx666 <global300@bk.ru>
2017-09-23 00:57:15 +0300

    Add files via upload

---
+++ Wildmoney/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Wildmoney
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(2, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(119, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(2, 64);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(119, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(2, 103);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(119, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(-1, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Login:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-1, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-1, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Post ID";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 30);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(172, 87);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(138, 20);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.Visible = false;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(168, 61);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(142, 20);
+            this.dateTimePicker1.TabIndex = 9;
+            this.dateTimePicker1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(316, 163);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(332, 202);
+            this.MinimumSize = new System.Drawing.Size(332, 202);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WILDMONEY TIME";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ Wildmoney/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+
+namespace Wildmoney
+{
+    public partial class Form1 : Form
+    {
+        IWebDriver Browser;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void Authorization()
+        {
+            Browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+            Browser.Manage().Window.Maximize();
+            Browser.Navigate().GoToUrl("https://wildmoney.org/login");
+            
+            WebDriverWait lp = new WebDriverWait(Browser, TimeSpan.FromSeconds(30)); //Задаем время ожидания 
+            IWebElement vv = lp.Until(ExpectedConditions.ElementIsVisible(By.Id("ctrl_pageLogin_login"))); //Задаем селектор , который должен появится на странице
+           
+            IWebElement login = Browser.FindElement(By.Id("ctrl_pageLogin_login")); //Поиск строки ввода логина
+            login.SendKeys("" + textBox1.Text); //Читаем логин из текстбокса и вводим 
+
+            IWebElement password = Browser.FindElement(By.Id("ctrl_pageLogin_password")); //Поиск строки ввода пароля
+            password.SendKeys("" + textBox2.Text + OpenQA.Selenium.Keys.Enter); //Читаем пароль из текстбокса и вводим , надимаем кнопку ентер для входа
+
+            System.Threading.Thread.Sleep(10000);
+        }
+     public  void MainMethod ()
+            {
+            System.Threading.Timer timer = new System.Threading.Timer(
+              new System.Threading.TimerCallback((o) =>
+              {
+                  textBox4.Invoke(new Action(() => textBox4.Text = DateTime.Now.ToString()));
+              }), null, 0, 1000);
+
+
+            int n = 1;
+            while (n< 6)
+            {
+                System.Threading.Thread.Sleep(30000);
+                string edit = "/edit";
+        Browser.Navigate().GoToUrl("https://wildmoney.org/profile-posts/" +textBox3.Text  + edit);
+        //Work
+
+
+        IWebElement post = Browser.FindElement(By.Name("message"));
+        post.Click();
+                post.Clear();
+
+               
+                post.SendKeys("" + textBox4.Text);
+
+                
+              
+
+                IWebElement go = Browser.FindElement(By.CssSelector(".primary"));
+        go.Click();
+
+              
+            }
+}
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Authorization();
+
+            Thread t = new Thread(MainMethod);
+            t.Start();
+
+            // textBox4.Text = dateTimePicker1.Value.ToString();
+
+            // dateTimePicker1.Value = DateTime.Now.ToLocalTime();
+
+          
+
+           
+
+        }
+
+       
+
+        
+         
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+   
+          
+            
+        }
+
+        
+    }
+
