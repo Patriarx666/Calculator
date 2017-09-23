7585521058ab644a230c2af6734dfdcd49334ac8
Patriarx666 <global300@bk.ru>
2017-09-23 19:09:04 +0300

    Add files via upload

---
+++ WindowsFormsApplication8/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Threading;
+
+
+namespace WindowsFormsApplication8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool isRunning = true;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread TH = new Thread(Keyboardd);
+            TH.SetApartmentState(ApartmentState.STA);
+            CheckForIllegalCrossThreadCalls = false;
+            TH.Start();
+        }
+
+        void Keyboardd()
+        {
+            while (isRunning)
+            {
+                Thread.Sleep(40);
+                if ((Keyboard.GetKeyStates(Key.LeftAlt) & KeyStates.Down) > 0)
+                {
+                    label1.Text = "Pressed";
+                }
+                else
+                {
+                    label1.Text = "Not pressed";
+                }
+
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            isRunning = false;
+        }
+    }
+}
